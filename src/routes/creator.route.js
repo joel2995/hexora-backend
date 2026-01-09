8ac698e60bc97ced19fcb222b56df1c543ea2aec
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-10 01:09:20 +0530

    creator route updation

--- src/routes/creator.route.js
+++ src/routes/creator.route.js
@@ -5,8 +5,10 @@ import Reputation from "../models/Reputation.model.js";
 const router = express.Router();
 
 router.get("/dashboard/:wallet", async (req, res) => {
-  const creator = await Creator.findOne({ wallet: req.params.wallet });
-  const reputation = await Reputation.findOne({ wallet: req.params.wallet });
+  const wallet = req.params.wallet.toLowerCase();
+
+  const creator = await Creator.findOne({ wallet });
+  const reputation = await Reputation.findOne({ wallet });
 
   res.json({ creator, reputation });
 });
