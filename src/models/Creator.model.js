635f38e0f961e909a715d08315e3886cf540c003
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-10 00:49:24 +0530

    creator model updation

--- src/models/Creator.model.js
+++ src/models/Creator.model.js
@@ -1,10 +1,35 @@
 import mongoose from "mongoose";
 
-const CreatorSchema = new mongoose.Schema({
-  wallet: String,
-  creatorToken: String,
-  ipfsCID: String,
-  updatedAt: Date
-});
+const CreatorSchema = new mongoose.Schema(
+  {
+    wallet: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+
+    creatorToken: {
+      type: String, // ERC20 address
+      index: true,
+    },
+
+    ipfsCID: {
+      type: String,
+    },
+
+    reputationRef: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Reputation",
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Creator", CreatorSchema);
