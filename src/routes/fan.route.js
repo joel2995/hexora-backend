629cf46664534005398c44cfdf279928abd4bc59
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-10 01:09:41 +0530

    fan route updation

--- src/routes/fan.route.js
+++ src/routes/fan.route.js
@@ -4,7 +4,7 @@ import Creator from "../models/Creator.model.js";
 const router = express.Router();
 
 router.get("/creators", async (_, res) => {
-  const creators = await Creator.find();
+  const creators = await Creator.find({ isActive: true });
   res.json(creators);
 });
 
