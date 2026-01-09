5db61a2734b16d43823cf332ff3b88fda9177fa8
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-10 01:42:02 +0530

    bypass signature

--- src/services/wallet.service.js
+++ src/services/wallet.service.js
@@ -1,6 +1,11 @@
 import { ethers } from "ethers";
 
 export function verifySignature(wallet, signature) {
+  // DEV MODE BYPASS (Postman testing only)
+  if (process.env.NODE_ENV === "development") {
+    return true;
+  }
+
   const message = "Login to HEXORA";
   const recovered = ethers.verifyMessage(message, signature);
   return recovered.toLowerCase() === wallet.toLowerCase();
