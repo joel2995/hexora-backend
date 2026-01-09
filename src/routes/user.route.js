13ca5b136634ec890b5f1bf1c9512bcbfbf7e33d
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-10 01:11:54 +0530

    user route updation

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,8 @@ import { detectRole } from "../services/role.service.js";
 const router = express.Router();
 
 router.get("/role/:wallet", async (req, res) => {
-  const role = await detectRole(req.params.wallet);
+  const wallet = req.params.wallet.toLowerCase();
+  const role = await detectRole(wallet);
   res.json(role);
 });
 
