a653a926014a1e2b29db8b664b2f7989b6cf6837
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-10 00:54:51 +0530

    utils updation

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,5 +1,8 @@
 import { keccak256, toUtf8Bytes } from "ethers";
 
 export function keccakHash(data) {
+  if (typeof data !== "string") {
+    data = JSON.stringify(data);
+  }
   return keccak256(toUtf8Bytes(data));
 }
