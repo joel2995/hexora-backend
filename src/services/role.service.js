b6e2809a1677e001270f5751ad338200faa3f7d9
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-10 00:55:20 +0530

    role service updation

--- src/services/role.service.js
+++ src/services/role.service.js
@@ -1,12 +1,13 @@
 import { ethers } from "ethers";
-import { getTokenFactory } from "./blockchain.service.js";
+import { shardeum } from "./blockchain/index.js";
 
 export async function detectRole(wallet) {
-  const factory = await getTokenFactory();
+  const factory = await shardeum.getTokenFactory();
   const token = await factory.creatorToToken(wallet);
 
   if (token !== ethers.ZeroAddress) {
     return { role: "creator", creatorToken: token };
   }
+
   return { role: "fan" };
 }
