f53c98f5385973ebec33700fcb16c6f533728511
joel2995 <joelsundarsingh2005@gmail.com>
2026-01-10 00:43:39 +0530

    db updation

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-  if (!process.env.MONGODB_URI) return;
+  if (!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI not set");
+  }
+
   await mongoose.connect(process.env.MONGODB_URI);
   console.log("MongoDB connected");
 }
